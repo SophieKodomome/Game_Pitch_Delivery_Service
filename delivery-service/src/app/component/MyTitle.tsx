0f69cd8c91ed02a4ac2ce8be0db6ffa585ce6d8f
Peeter Randria <126390972+SophieKodomome@users.noreply.github.com>
2025-03-02 18:45:04 +0300

    add fonts

---
+++ delivery-service/src/app/component/MyTitle.tsx
@@ -0,0 +1,11 @@
+export default function MyTitle() {
+    return (
+      <h1
+        className="stroke-2 text-[var(--my-white)] font-keyute text-8xl"
+        style={{ stroke: "var(--my-primary)" }} // ✅ Add stroke using inline style
+      >
+        Very Daily Delivery
+      </h1>
+    );
+  }
+  
